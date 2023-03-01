a7a0ae9dc9b983596119cfcd7bccb8d682ec7868
sarah-zhan <yizh.contact@gmail.com>
2023-03-01 14:39:31 -0800

    fix new tweet form

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,18 +1,19 @@
-// let textArea = document.querySelector("#tweet-text");
-// let characterCounter = document.querySelector(".counter");
 const maxNumberOfChar = 140;
 
-$(document).ready(() => {
-  $(function() {
-    let numberOfText = $('textArea').text().value.length;
-    let counter = maxNumberOfChar - numberOfText;
-    $('.counter').text() = counter;
-    $('textArea').on('change', characterCounter);
-    if (counter < 0) {
-      $('.counter').style.color = 'red';
+$(function() {
+  $('textarea').on('input', () => {
+    const $textArea = $("#tweet-text").val();
+    const $numberOfText = $textArea.length;
+    const $counter = maxNumberOfChar - $numberOfText;
+    $('.counter').text($counter);
+    if ($counter < 0) {
+      $('.counter').addClass('red');
+    } else {
+      $('.counter').removeClass('red');
     }
   });
-})
+});
+
 
 
   
